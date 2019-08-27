5cd136fbac6c129af1ff3ce854f5f6acfd607bec
刘成 <lttsd@foxmail.com>
2019-08-27 19:12:17 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -2,8 +2,9 @@ const assert = require('assert');
 
 describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js');
+
   it('entry', () => {
-    assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js') > -1, true);
-    assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js') > -1, true);
+    assert.equal(baseConfig.entry.index.indexOf('builder-webpack-a/test/smoke/template/src/index/index.js') > -1, true);
+    assert.equal(baseConfig.entry.search.indexOf('builder-webpack-a/test/smoke/template/src/search/index.js') > -1, true);
   });
 });
